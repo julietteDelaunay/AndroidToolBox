ba93b7216adc2032accda677c1bbdcc650b563e9
julietteDelaunay <juliette.delaunay@isen.yncrea.fr>
2020-02-21 15:30:29 +0100

    ajout lifeCycle et appui sur bouton cycle de vie

--- app/src/main/java/fr/isen/delaunay/androidtoolbox/HomeActivity.kt
+++ app/src/main/java/fr/isen/delaunay/androidtoolbox/HomeActivity.kt
@@ -1,12 +1,24 @@
 package fr.isen.delaunay.androidtoolbox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_login.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+            //si le bouton image ( cycle de vie) est activée
+        imgCycleDeVie.setOnClickListener(){
+                //pour demarer l'activitée CycleLife
+                val intent = Intent(this, CycleLife::class.java)
+                startActivity(intent)
+
+        }
     }
 }
--- app/src/main/java/fr/isen/delaunay/androidtoolbox/LoginActivity.kt
+++ app/src/main/java/fr/isen/delaunay/androidtoolbox/LoginActivity.kt
@@ -1,12 +1,39 @@
 package fr.isen.delaunay.androidtoolbox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        buttonvalidate.setOnClickListener(){
+            var idetifiantUser :String = editID.text.toString()
+            var passwordUser : String = editPSW.text.toString()
+            //q d
+            //Toast.makeText(applicationContext,"votre ID est: ${editID.text.toString()}",Toast.LENGTH_SHORT).show()
+            //question e
+            if (idetifiantUser == "admin"&& passwordUser== "123")
+            {
+                //pour demarer l'activitée HomeActivity
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                //dire que l'ID et le pswd est valide
+            //Toast.makeText(applicationContext,"votre ID est: ${idetifiantUser}",Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                Toast.makeText(applicationContext,"votre ID ou votre password est mauvais ",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
     }
+
 }
