0495ae0f707bc9d7ef6d4d9fbb29fd5654bb539e
julietteDelaunay <juliette.delaunay@isen.yncrea.fr>
2020-03-06 08:16:30 +0100

    ajout lifeCycle et appui sur bouton cycle de vie

--- app/src/main/java/fr/isen/delaunay/androidtoolbox/CycleLife.kt
+++ app/src/main/java/fr/isen/delaunay/androidtoolbox/CycleLife.kt
@@ -2,11 +2,53 @@ package fr.isen.delaunay.androidtoolbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_cycle_life.*
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_home.textCycle
 
 class CycleLife : AppCompatActivity() {
-
+var texte :String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cycle_life)
+        texte += "app create\n"
+        textCycle.text= texte
+    }
+
+    override fun onStart() {
+        super.onStart()
+        texte += "app start\n"
+        textCycle.text= texte
+    }
+    override fun onPause() {
+        super.onPause()
+        texte += "app pause\n"
+        textCycle.text= texte
+        Toast.makeText(applicationContext,"app pause",Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        texte += "app resume\n"
+        textCycle.text= texte
+        Toast.makeText(applicationContext,"app Resume",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        texte += "app stop\n"
+        textCycle.text= texte
+        Toast.makeText(applicationContext,"app stop",Toast.LENGTH_SHORT).show()
+        Log.i("stop" ,  "en stop")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(applicationContext,"app Destroy",Toast.LENGTH_SHORT).show()
+        Log.i("destroy" ,  "Destroy")
     }
 }
