3a369b5935e428a3c5228d3712d9c64ae888a7c9
julietteDelaunay <juliette.delaunay@isen.yncrea.fr>
2020-03-20 11:55:06 +0100

    initialisation BLE

--- app/src/main/java/fr/isen/delaunay/androidtoolbox/BLEScanActivity.kt
+++ app/src/main/java/fr/isen/delaunay/androidtoolbox/BLEScanActivity.kt
@@ -1,12 +1,97 @@
 package fr.isen.delaunay.androidtoolbox
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.View
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_b_l_e_scan.*
 
 class BLEScanActivity : AppCompatActivity() {
+
+    companion object {
+        private val REQUEST_ENABLE_BT = 1
+        private val SCAN_PERIOD: Long = 4000
+    }
+
+    private val bluetoothAdapter: BluetoothAdapter? by lazy(LazyThreadSafetyMode.NONE) {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
+    private val isBLEEnable: Boolean
+        get() = bluetoothAdapter?.isEnabled == true
+
+    private lateinit var handler: Handler
+    private var mScanning: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_b_l_e_scan)
+
+        when {
+            isBLEEnable -> initScan()
+            bluetoothAdapter != null -> {
+                // demande d'activation bluetooth
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+            }
+            else -> {
+                // device pas compatible BLE
+                dividerBle.visibility = View.VISIBLE
+            }
+        }
+
     }
+
+    override fun onStop() {
+        super.onStop()
+        if (isBLEEnable) {
+            scanLeDevice(false)
+        }
+    }
+
+    private fun initScan() {
+        progressBar.visibility = View.VISIBLE
+        txtBLE.visibility = View.GONE
+
+        handler = Handler()
+        scanLeDevice(true)
+    }
+
+    private fun scanLeDevice(enable: Boolean) {
+        bluetoothAdapter?.bluetoothLeScanner?.apply {
+            if (enable) {
+                handler.postDelayed({
+                    mScanning = false
+                    stopScan(leScanCallBack)
+                }, SCAN_PERIOD)
+                mScanning = true
+                startScan(leScanCallBack)
+            } else {
+                mScanning = false
+                stopScan(leScanCallBack)
+            }
+        }
+    }
+
+    private val leScanCallBack = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult?) {
+            super.onScanResult(callbackType, result)
+            Log.w("BLEScanActivity", "${result?.device}")
+            runOnUiThread {
+                txtBLE.visibility = View.GONE
+            }
+        }
+    }
+
 }
+
+
+
---
+++ app/src/main/java/fr/isen/delaunay/androidtoolbox/WebServiceActivity.kt
@@ -0,0 +1,12 @@
+package fr.isen.delaunay.androidtoolbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WebServiceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_service)
+    }
+}
