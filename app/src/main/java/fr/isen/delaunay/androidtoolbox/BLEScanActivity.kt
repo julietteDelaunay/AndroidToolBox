ed0ada1650d9896333a326b02971dd5af4403467
julietteDelaunay <juliette.delaunay@isen.yncrea.fr>
2020-03-25 10:51:54 +0100

    BLE+adapterBLE

--- app/src/main/java/fr/isen/delaunay/androidtoolbox/BLEScanActivity.kt
+++ app/src/main/java/fr/isen/delaunay/androidtoolbox/BLEScanActivity.kt
@@ -1,6 +1,10 @@
 package fr.isen.delaunay.androidtoolbox
 
+//import android.annotation.SuppressLint
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
@@ -11,55 +15,64 @@ import android.os.Handler
 import android.util.Log
 import android.view.View
 import android.content.Intent
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_b_l_e_scan.*
+import kotlin.collections.ArrayList
+
 
 class BLEScanActivity : AppCompatActivity() {
 
-    companion object {
-        private val REQUEST_ENABLE_BT = 1
-        private val SCAN_PERIOD: Long = 4000
-    }
+    private lateinit var handler: Handler
+    private var mScanning: Boolean = false
+    private lateinit var adapter: BLEAdapterActivity
+    private val devices = ArrayList<ScanResult>()
 
     private val bluetoothAdapter: BluetoothAdapter? by lazy(LazyThreadSafetyMode.NONE) {
         val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
         bluetoothManager.adapter
     }
 
-    private val isBLEEnable: Boolean
+    private val isBLEEnabled: Boolean
         get() = bluetoothAdapter?.isEnabled == true
-
-    private lateinit var handler: Handler
-    private var mScanning: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_b_l_e_scan)
 
-        when {
-            isBLEEnable -> initScan()
-            bluetoothAdapter != null -> {
-                // demande d'activation bluetooth
-                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
-            }
-            else -> {
-                // device pas compatible BLE
-                dividerBle.visibility = View.VISIBLE
+        bleTextfailed.visibility = View.GONE
+
+        BLErecycler.adapter = BLEAdapterActivity(devices, ::onDeviceClicked)
+        BLErecycler.layoutManager = LinearLayoutManager(this)
+        textScanBLE.setOnClickListener{
+            when {
+                isBLEEnabled -> {
+                    //init scan
+                    initBLEScan()
+                    initScan()
+
+                }
+                bluetoothAdapter != null -> {
+                    //ask for permission
+                    val enableBTIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                    startActivityForResult(enableBTIntent, REQUEST_ENABLE_BT)
+                }
+                else -> {
+                    //device is not compatible with your device
+                    bleTextfailed.visibility = View.VISIBLE
+                }
             }
         }
 
     }
 
-    override fun onStop() {
-        super.onStop()
-        if (isBLEEnable) {
-            scanLeDevice(false)
-        }
-    }
+
 
     private fun initScan() {
         progressBar.visibility = View.VISIBLE
-        txtBLE.visibility = View.GONE
+        dividerBle.visibility = View.GONE
 
         handler = Handler()
         scanLeDevice(true)
@@ -67,31 +80,107 @@ class BLEScanActivity : AppCompatActivity() {
 
     private fun scanLeDevice(enable: Boolean) {
         bluetoothAdapter?.bluetoothLeScanner?.apply {
+
             if (enable) {
+                Toast.makeText(applicationContext, "scann le device lancé ", Toast.LENGTH_SHORT).show()
+                Log.w("BLEScanActivity", "Scanning for devices")
                 handler.postDelayed({
+                    Log.w("BLEScanActivity", "part 1")
                     mScanning = false
-                    stopScan(leScanCallBack)
+                    stopScan(leScanCallback)
                 }, SCAN_PERIOD)
                 mScanning = true
-                startScan(leScanCallBack)
+                startScan(leScanCallback)
+                adapter.clearResults()
+                adapter.notifyDataSetChanged()
+                progressBar.progress = (progressBar.progress + 5) % 100
+                Log.w("BLEScanActivity", "part 0")
+                togglePlayPauseAction()
             } else {
+                Log.w("BLEScanActivity", "part 2")
                 mScanning = false
-                stopScan(leScanCallBack)
+                stopScan(leScanCallback)
             }
         }
     }
 
-    private val leScanCallBack = object : ScanCallback() {
-        override fun onScanResult(callbackType: Int, result: ScanResult?) {
-            super.onScanResult(callbackType, result)
-            Log.w("BLEScanActivity", "${result?.device}")
-            runOnUiThread {
-                txtBLE.visibility = View.GONE
+   private val leScanCallback = object : ScanCallback() {
+       override fun onScanResult(callbackType: Int, result: ScanResult) {
+           super.onScanResult(callbackType, result)
+           Log.w("BLEScanActivity", "onResult()")
+           Log.w("BLEScanActivity", "CaAMarche ${result?.device}")
+           runOnUiThread {
+               adapter.addDeviceToList(result)
+               adapter.notifyDataSetChanged()
+           }
+       }
+   }
+
+       private fun initBLEScan() {
+        adapter = BLEAdapterActivity(
+            arrayListOf(),
+            ::onDeviceClicked
+        )
+        BLErecycler.adapter = adapter
+        BLErecycler.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
+
+        handler = Handler()
+
+        scanLeDevice(true)
+        textScanBLE.setOnClickListener{
+        Toast.makeText(this, "scann lancé", Toast.LENGTH_SHORT).show()
+            scanLeDevice(!mScanning)
+      }
+
+    }
+
+    private fun onDeviceClicked(device: BluetoothDevice) {
+        val intent = Intent(this, BLEAdapterActivity::class.java)
+        intent.putExtra("ble_device", device)
+        startActivity(intent)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (isBLEEnabled) {
+                    Toast.makeText(this, "Bluetooth has been enabled", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Bluetooth has been disabled", Toast.LENGTH_SHORT).show()
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(this, "Bluetooth enabling has been canceled", Toast.LENGTH_SHORT)
+                    .show()
             }
         }
     }
+
+    companion object {
+        private const val SCAN_PERIOD: Long = 10000
+        private const val REQUEST_ENABLE_BT = 44
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun togglePlayPauseAction(){
+        if(mScanning){
+            progressBar.visibility= View.VISIBLE
+            dividerBle.visibility= View.INVISIBLE
+            textScanBLE.text = "scan BLE en cours"
+            imgPlay.setImageDrawable(ContextCompat.getDrawable(this,android.R.drawable.ic_media_pause))
+        }
+        else {
+            progressBar.visibility= View.INVISIBLE
+            dividerBle.visibility= View.VISIBLE
+            textScanBLE.text = "lancer le scan BLE"
+            imgPlay.setImageDrawable(ContextCompat.getDrawable(this,android.R.drawable.ic_media_play))
+
+        }
+    }
+
 
 }
+
 
 
 
--- app/src/main/java/fr/isen/delaunay/androidtoolbox/ContactAdapter.kt
+++ app/src/main/java/fr/isen/delaunay/androidtoolbox/ContactAdapter.kt
@@ -4,21 +4,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_permission_cell.view.*
 
 
 class  ContactAdapter(val contact : List<String>) : RecyclerView.Adapter< ContactAdapter.ContactViewHolder>() {
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder =
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder =
         ContactViewHolder(
             LayoutInflater.from(parent.context)
                 .inflate(R.layout.activity_permission_cell, parent, false)
             //permet de cree le Layout et le Viewholder associé
             //Viewholder va gerer la vue.
+
         )
 
+
     override fun getItemCount(): Int {
+
         return contact.size
         // ou on enlève les crochets et met = notreInt
     }
